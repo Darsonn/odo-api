a03475e6323998775a98f8680427900a500850b5
Darsonn <87154431+Darsonn@users.noreply.github.com>
2026-05-01 03:15:14 +0200

    chore: initialize Spring Boot project with dependencies

---
+++ src/main/java/pl/darsonn/odoapi/OdoApiApplication.java
@@ -0,0 +1,13 @@
+package pl.darsonn.odoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdoApiApplication.class, args);
+    }
+
+}
